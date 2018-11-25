6dd7411c7f65ceb6a17c2dce38459fdd13f11b1e
Alexander Kudryashov <a.kudryashov.85@gmail.com>
2018-11-25 23:21:22 +0400

    Some refactors

--- src/graphql/resolvers/Catalog.js
+++ src/graphql/resolvers/Catalog.js
@@ -2,7 +2,7 @@ module.exports = {
 	company: async (catalog, args, { Company }) => {
 		return await Company.findOne({ _id: catalog.companyId })
 	},
-	images: async (catalog, args, { Image }) => {
-		return await Image.find({ _id: { $in: catalog.imagesIds } }).sort({ catalogIndex: 1 })
+	images: async (catalog, { limit }, { Image }) => {
+		return await Image.find({ _id: { $in: catalog.imagesIds } }).sort({ catalogIndex: 1 }).limit(limit)
 	}
 }
--- src/service/config.js
+++ src/service/config.js
@@ -6,15 +6,18 @@ dotenv.config({ path: root('.env') })
 
 const isProd = process.env.NODE_ENV === 'production'
 
-exports.IS_DEV = !isProd
-exports.HOST = process.env.HOST
-exports.PORT = process.env.PORT
-exports.ENDPOINT_PATH = process.env.ENDPOINT_PATH
-exports.MONGO_URL = process.env.MONGO_URL
-exports.COOKIE_SECRET = process.env.COOKIE_SECRET
-exports.ADMIN_USERNAME = process.env.ADMIN_USERNAME
-exports.ADMIN_PASSWORD = process.env.ADMIN_PASSWORD
-exports.JWT_SECRET = process.env.JWT_SECRET
-exports.JWT_EXPIRE = isProd ? +process.env.JWT_EXPIRE : 60 * 60 * 24 * 365
-exports.UPLOAD_DIR = process.env.UPLOAD_DIR
-exports.CATALOGS_DIR = process.env.CATALOGS_DIR
+module.exports = {
+	IS_DEV: !isProd,
+	HOST: process.env.HOST,
+	PORT: process.env.PORT,
+	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+	ENDPOINT_PATH: process.env.ENDPOINT_PATH,
+	MONGO_URL: process.env.MONGO_URL,
+	COOKIE_SECRET: process.env.COOKIE_SECRET,
+	ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+	ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+	JWT_SECRET: process.env.JWT_SECRET,
+	JWT_EXPIRE: isProd ? +process.env.JWT_EXPIRE : 60 * 60 * 24 * 365,
+	UPLOAD_DIR: process.env.UPLOAD_DIR,
+	CATALOGS_DIR: process.env.CATALOGS_DIR
+}
